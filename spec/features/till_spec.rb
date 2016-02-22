5592bb0b335f6355a4b5b5f9f828d3e9ef5ee306
Chris Wynne <chris.wynndow@gmail.com>
2016-02-22 11:54:06 +0000

    Adds feature and unit tests till

---
+++ spec/features/till_spec.rb
@@ -0,0 +1,13 @@
+require 'till.rb'
+
+describe 'Till Features' do
+
+  it 'Is a thing' do
+    expect(Till).to respond_to(:new)
+  end
+
+  it 'know the menu' do
+
+  end
+
+end
---
+++ spec/till_spec.rb
@@ -0,0 +1,11 @@
+describe Till do
+
+  subject(:till) {described_class.new}
+
+  it 'can exist' do
+    expect(till.class).to be (Till)
+  end
+
+
+
+end
